6f02cd8ad7bea21f6d906ce3b202b27e6d5e25f9
clownkill <alehnovich90@yandex.ru>
2021-12-31 11:26:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import requests
 import os
 from dotenv import load_dotenv
+from itertools import count
 from terminaltables import AsciiTable
 
 
-def predict_rub_salary_hh(vacancie):
-    salary = vacancie['salary']
+def predict_rub_salary_hh(vacancy):
+    salary = vacancy['salary']
     if not salary or salary['currency'] != 'RUR':
         return None
     return predict_salary(salary['from'], salary['to'])
 
 
-def predict_rub_salary_sj(vacancie):
-    if vacancie['currency'] != 'rub':
+def predict_rub_salary_sj(vacancy):
+    if vacancy['currency'] != 'rub':
         return None
-    salary_from = vacancie['payment_from']
-    salary_to = vacancie['payment_to']
+    salary_from = vacancy['payment_from']
+    salary_to = vacancy['payment_to']
     return predict_salary(salary_from, salary_to)
 
 
@@ -24,88 +25,130 @@ def predict_salary(salary_from, salary_to):
         return (salary_from + salary_to) / 2
     elif salary_from and not salary_to:
         return salary_from * 1.2
-    elif not salary_from and salary_to:
-        return salary_to * 0.8
-    return None
+    return salary_to * 0.8
+
+
+def get_hh_salary_statictics(vacancies):
+    salaries = []
+    processed_vacancies = 0
+    for vacancy in vacancies:
+        salary = predict_rub_salary_hh(vacancy)
+        if salary:
+            salaries.append(salary)
+            processed_vacancies += 1
+    return salaries, processed_vacancies
+
+
+def get_sj_salary_statistics(vacancies):
+    salaries = []
+    processed_vacancies = 0
+    for vacancy in vacancies:
+        salary = predict_rub_salary_sj(vacancy)
+        if salary:
+            salaries.append(salary)
+            processed_vacancies += 1
+    return salaries, processed_vacancies
 
 
 def get_hh_statistics(languages):
-    url_hh = 'https://api.hh.ru/vacancies'
+    url = 'https://api.hh.ru/vacancies'
     languages_stat = {}
     for language in languages:
-        languages_stat[language] = {}
-        page = 0
+        vacancies = []
         pages = 1
-        vacancies, salaries = [], []
-        vacancies_proceed = 0
-        while page < pages:
-            params_hh = {
+        vacancies_quantity = 0
+        params = {
                 'text': f'программист {language}',
                 'area.name': 'Moscow',
                 'period': 30,
-                'page': page,
             }
-            vacancies_data = requests.get(url_hh, params=params_hh).json()
-            languages_stat[language]['vacancies_found'] = vacancies_data['found']
-            for vacancie_info in vacancies_data['items']:
-                vacancies.append(vacancie_info)
-            page += 1
-            pages = vacancies_data['pages']
-        for vacancie in vacancies:
-            salary = predict_rub_salary_hh(vacancie)
-            if salary:
-                salaries.append(salary)
-                vacancies_proceed += 1
-        languages_stat[language]['vacancies_proceed'] = vacancies_proceed
-        languages_stat[language]['average_salary'] = int(sum(salaries) / vacancies_proceed)
+        for page in count(0, 1):
+            params['page'] = page
+            if page == pages:
+                break
+            try:
+                response = requests.get(url, params=params)
+                response.raise_for_status()
+                responsed_vacansies = response.json()
+                vacancies_quantity = responsed_vacansies['found']
+                vacancies.extend(responsed_vacansies['items'])
+                pages = responsed_vacansies['pages']
+            except requests.exceptions.HTTPError:
+                print(f'Ошибка получения данных по языку {language} от hh.ru')
+        salaries, processed_vacancies = get_hh_salary_statictics(vacancies)
+        try:
+            average_salary = int(sum(salaries) / processed_vacancies)
+        except ZeroDivisionError:
+            average_salary = 0
+        languages_stat[language] = {
+            'vacancies_found': vacancies_quantity,
+            'processed_vacancies': processed_vacancies,
+            'average_salary': average_salary,
+        }
     return languages_stat
 
 
 def get_sj_statistics(apikey, languages):
-    url_sj = 'https://api.superjob.ru/2.0/vacancies/'
+    url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id': apikey,
     }
     languages_stat = {}
     for language in languages:
-        languages_stat[language] = {}
-        page = 0
-        vacancies_proceed = 0
-        vacancies, salaries = [], []
-        more = True
-        while more:
-            params = {
+        vacancies = []
+        vacancies_quantity = 0
+        params = {
                 'keyword': f'программист {language}',
                 'town': 'москва',
                 'count': 100,
-                'page': page,
-                'more': more,
             }
-            vacancies_data = requests.get(url_sj, headers=headers, params=params).json()
-            languages_stat[language]['vacancies_found'] = vacancies_data['total']
-            for vacancie_info in vacancies_data['objects']:
-                vacancies.append(vacancie_info)
-            page += 1
-            more = vacancies_data['more']
-        for vacancie in vacancies:
-            salary = predict_rub_salary_sj(vacancie)
-            if salary:
-                salaries.append(salary)
-                vacancies_proceed += 1
-        languages_stat[language]['vacancies_proceed'] = vacancies_proceed
-        languages_stat[language]['average_salary'] = int(sum(salaries) / vacancies_proceed)
+        for page in count(0, 1):
+            more = True
+            params['page'] = page
+            params['more'] = more
+            try:
+                response = requests.get(url, headers=headers, params=params)
+                response.raise_for_status()
+                responsed_vacansies = response.json()
+                vacancies_quantity = responsed_vacansies['total']
+                vacancies.extend(responsed_vacansies['objects'])
+                more = responsed_vacansies['more']
+            except requests.exceptions.HTTPError:
+                print(f'Ошибка получения данных по языку {language} \
+                от superjob.ru')
+            if not more:
+                break
+        salaries, processed_vacancies = get_sj_salary_statistics(vacancies)
+        try:
+            average_salary = int(sum(salaries) / processed_vacancies)
+        except ZeroDivisionError:
+            average_salary = 0
+        languages_stat[language] = {
+            'vacancies_found': vacancies_quantity,
+            'processed_vacancies': processed_vacancies,
+            'average_salary': average_salary,
+        }
     return languages_stat
 
 
 def print_table(languages_stat, table_name):
     table_data = (
-        ('Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'),
+        (
+            'Язык программирования',
+            'Вакансий найдено',
+            'Вакансий обработано',
+            'Средняя зарплата'
+        ),
     )
     for name, info in languages_stat.items():
-        table_data += (name, info['vacancies_found'], info['vacancies_proceed'], info['average_salary']),
+        table_data += (
+                          name,
+                          info['vacancies_found'],
+                          info['processed_vacancies'],
+                          info['average_salary']
+                      ),
     table_instance = AsciiTable(table_data, table_name)
-    print('\n', table_instance.table, '\n')
-
+    return f'\n {table_instance.table} \n'
 
 
 def main():
@@ -125,8 +168,8 @@ def main():
     ]
     hh_statistic = get_hh_statistics(languages)
     sj_statistic = get_sj_statistics(sj_apikey, languages)
-    print_table(hh_statistic, 'HeadHunter Moscow')
-    print_table(sj_statistic, 'SuperJob Moscow')
+    print(print_table(hh_statistic, 'HeadHunter Moscow'))
+    print(print_table(sj_statistic, 'SuperJob Moscow'))
 
 
 if __name__ == '__main__':
