617e8d821d52b00ec1e9b27727a030417ee7e9bf
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-01-05 22:04:50 +0300

    Поправлен расчет средней зарплаты

--- main.py
+++ main.py
@@ -21,11 +21,14 @@ def predict_rub_salary_sj(vacancy):
 
 
 def predict_salary(salary_from, salary_to):
-    if salary_from and salary_to:
-        return (salary_from + salary_to) / 2
+    if not salary_from and salary_to:
+        return salary_to * 0.8
     elif salary_from and not salary_to:
         return salary_from * 1.2
-    return salary_to * 0.8
+    elif salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+
+
 
 
 def get_hh_salary_statictics(vacancies):
